a9f7e97123472231e76016d7ecae8439c85a9449
Poralia <poraliaglobal@gmail.com>
2023-12-14 10:04:47 +0700

    User email instead username on user creation inside Django Dashboard

--- logiblue/apps/user/admin.py
+++ logiblue/apps/user/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .forms import UserCreationFormExtend
+
+
+class UserAdminExtend(UserAdmin):
+    add_form = UserCreationFormExtend
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdminExtend)
---
+++ logiblue/apps/user/forms.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
+from django import forms
+
+from .models import User
+
+
+class UserCreationFormExtend(UserCreationForm):
+    email = forms.EmailField(help_text=_("Enter valid email address."))
+
+    def clean_email(self):
+        """Reject emails that differ only in case."""
+        email = self.cleaned_data.get("email")
+        if (
+            email
+            and self._meta.model.objects.filter(email__iexact=email).exists()
+        ):
+            self._update_errors(
+                ValidationError(
+                    {
+                        "email": self.instance.unique_error_message(
+                            self._meta.model, ["email"]
+                        )
+                    }
+                )
+            )
+        else:
+            return email
+
+    def save(self, commit=True):
+        user = super().save(commit)
+        username = self.cleaned_data.get('username', None)
+        email = self.cleaned_data.get('email', None)
+
+        if username is None and email is not None:
+            username = User.objects.generate_username_from_email(email)
+            user.username = username
+            user.save()
+    
+        return user
