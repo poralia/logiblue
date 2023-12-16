dd25969f1a4da5f25bb5b74404e88aca83625bb9
Poralia <poraliaglobal@gmail.com>
2023-12-16 15:21:05 +0700

    added frontend cors

--- logiblue/logiblue/settings/production.py
+++ logiblue/logiblue/settings/production.py
@@ -7,6 +7,7 @@ ALLOWED_HOSTS = ['poralia.pythonanywhere.com']
 
 CORS_ALLOWED_ORIGINS = [
     'https://poralia.pythonanywhere.com',
+    'https://logiblue-fe.vercel.app',
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR.parent, 'static')
